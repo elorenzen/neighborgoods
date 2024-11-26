9312346ab95791f7a7ad804554723bbcf03540cf
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-11-26 15:01:12 -0800

    Begin views setup, install nuxt modules

---
+++ components/AppHeader.vue
@@ -0,0 +1,140 @@
+<template>
+    <Menubar>
+        <template #start>
+            <NuxtLink to="/" class="ml-2 text-xl font-bold">NeighborGoods</NuxtLink>
+        </template>
+        <template #end>
+            <div class="flex items-center gap-2">
+                <InputText v-if="!user" placeholder="Email" v-model="email" type="text" class="w-32 sm:w-auto" />
+                <Password v-if="!user" placeholder="Password" v-model="password" class="w-32 sm:w-auto" />
+                <Button
+                  v-if="!user"
+                  outlined
+                  :disabled="email == '' || password == ''"
+                  :loading="loading"
+                >Login</Button>
+  
+                <Button
+                  v-if="!user"
+                  outlined
+                  severity="secondary"
+                  @click="toSalePage"
+                >Post Your Sale</Button>
+  
+                <!-- <Button v-if="user" outlined severity="contrast" type="button" icon="pi pi-user" @click="toggleAccountMenu" aria-haspopup="true" aria-controls="account_menu" />
+                <Menu
+                  ref="menu"
+                  id="account_menu"
+                  :model="menuItems"
+                  :popup="true"
+                  :rerender="renderKey"
+                /> -->
+                <!-- <Avatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" shape="circle" /> -->
+            </div>
+        </template>
+    </Menubar>
+<!--   
+    <ErrorDialog v-if="errDialog" :errType="'Sign-in'" :errMsg="errMsg" @errorClose="errDialog = false" /> -->
+  </template>
+  
+  <script setup lang="ts">
+  const supabase  = useSupabaseClient()
+  const router    = useRouter()
+  const user      = useSupabaseUser()
+//   const store     = useUserStore()
+//   const storeUser = store.user
+  
+  const loading   = ref(false)
+  const errDialog = ref(false)
+  const errMsg    = ref()
+  const renderKey = ref(0)
+  
+  const email     = ref('')
+  const password  = ref('')
+  const menu      = ref();
+//   const menuItems = ref([
+//       {
+//           items: [
+//               {
+//                   label: 'Profile',
+//                   icon: 'pi pi-home',
+//                   command: () => {
+//                     router.push(`/${storeUser.type}s/${storeUser.associated_merchant_id ? storeUser.associated_merchant_id : storeUser.associated_vendor_id}`)
+//                   }
+//               },
+//               {
+//                   label: 'Messages',
+//                   icon: 'pi pi-inbox',
+//                   command: () => {
+//                     router.push(`/messages/${storeUser.associated_merchant_id ? storeUser.associated_merchant_id : storeUser.associated_vendor_id}`)
+//                   }
+//               },
+//               {
+//                   label: 'Settings',
+//                   icon: 'pi pi-cog',
+//                   command: () => {
+//                     router.push(`/settings/${storeUser.associated_merchant_id ? storeUser.associated_merchant_id : storeUser.associated_vendor_id}`)
+//                   }
+//               },
+//               {
+//                   label: `Sign out`,
+//                   icon: 'pi pi-sign-out',
+//                   command: async () => {
+//                     await signOut()
+//                   }
+//               }
+//           ]
+//       }
+//   ]);
+  
+  const toggleAccountMenu = (event: any) => {
+    menu.value.toggle(event)
+  }
+  const fireAuth = async () => {
+    loading.value = true
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+    })
+  
+    if (!error && data) {
+      const { data: userData } = await supabase
+        .from('users')
+        .select()
+        .eq('id', data.user.id)
+      const foundUser = userData ? userData[0] : null
+      await store.fetchUser(foundUser)
+      renderKey.value++
+  
+      if (foundUser && foundUser.type !== 'admin') {
+        await navigateTo(
+          foundUser.associated_merchant_id ?
+          `/merchants/${foundUser.associated_merchant_id}` :
+          `/vendors/${foundUser.associated_vendor_id}`   
+        )
+      } else if (foundUser && foundUser.type === 'admin') {
+        await navigateTo('/admin')
+      }
+    } else if (error) {
+      errDialog.value = true
+      errMsg.value = error.message
+    }
+    loading.value = false
+  }
+  
+  const register = async () => { await navigateTo('/signup') }
+  
+  const signOut = async () => {
+    console.log('signing out')
+    await supabase.auth.signOut()
+    email.value = ''
+    password.value = ''
+    await navigateTo('/')
+  }
+  const toSalePage = async () => {
+    await navigateTo('/createSale')
+  }
+  
+  </script>
+  
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module', '@nuxtjs/supabase'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false // set to 'true', EVENTUALLY
+  },
+  primevue: {
+    options: {
+          theme: {
+              preset: Aura
+          }
+      }
+  }
 })
