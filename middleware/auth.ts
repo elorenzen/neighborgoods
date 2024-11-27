e3f6bda24cab09470c0fbe0553479786f75bc87e
Eric Lorenzen <eric.lorenzen@gmail.com>
2024-11-27 10:04:56 -0800

    Add basic middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,11 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    const supabase = useSupabaseClient()
+    const user = useSupabaseUser()
+
+    // if user exists and is signed in, set user data in store
+    if (user.value) return
+    else {
+        await navigateTo('/')
+    }
+  })
+  
