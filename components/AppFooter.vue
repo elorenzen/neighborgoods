e17984eb2661d2447b370349018681a7329e7e25
Eric <eric.lorenzen@gmail.com>
2024-12-10 19:07:00 -0800

    Begin dev of footer nav

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="footer">
+        Footer stuff here
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+    background-color: #24b47e;
+    position:fixed;
+    bottom:0;
+    width: 100%;
+}
+</style>
